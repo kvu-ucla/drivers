ffd11aa51f1ff62dfa6b444adb46cf40835cef58
Kenneth Vu <kvu@it.ucla.edu>
2025-12-22 09:06:43 -0800

    (TESTING) updated shards for crestron interface

---
+++ drivers/crestron/cres_proc.cr
@@ -0,0 +1,71 @@
+require "placeos-driver"
+require "json"
+
+class Crestron::SIMPLInterface < PlaceOS::Driver
+  descriptive_name "Crestron - SIMPL Interface"
+  generic_name :CrestronInterface
+  tcp_port 9001
+
+  default_settings({normally_open: true})
+
+  @state : Bool? = nil
+
+  def on_load
+    queue.delay = 100.milliseconds
+    on_update
+  end
+
+  def on_update
+    publish_state
+  end
+
+  def connected
+    transport.tokenizer = Tokenizer.new("\r\n")
+    do_poll
+    schedule.every(50.seconds) do
+      logger.debug { "-- Polling Crestron Processor" }
+      do_poll
+    end
+  end
+
+  def do_poll
+    query
+  end
+
+  def query
+    send("query\r\n", name: "query")
+  end
+
+  def received(bytes : Bytes, task)
+    line = String.new(bytes)
+    data = JSON.parse(line)
+
+    incoming = data["digital-io1"]?.try &.as_bool?
+
+    if incoming.nil?
+      logger.warn { "unrecognized boolean payload: #{line.inspect}" }
+      task.try(&.abort)
+      return
+    end
+
+    if incoming != @state
+      @state = incoming
+      publish_state
+    end
+
+    task.try(&.success)
+  rescue error
+    logger.warn(exception: error) { "failed to process inbound state" }
+    task.try(&.abort)
+  end
+
+  def state : Bool?
+    @state
+  end
+
+  private def publish_state
+    val = @state
+    return if val.nil?
+    self[:state] = val.not_nil!
+  end
+end
---
+++ drivers/crestron/cres_proc_spec.cr
@@ -0,0 +1,15 @@
+require "placeos-driver/spec"
+
+DriverSpecs.mock_driver "Crestron::SIMPLInterface" do
+  # transmit some data
+  transmit(%({"digital-io1": true}\r\n))
+
+  # check that the state updated as expected
+  status[:state].should eq(true)
+
+  # transmit some data
+  transmit(%({"digital-io1": false}\r\n))
+
+  # check that the state updated as expected
+  status[:state].should eq(false)
+end
