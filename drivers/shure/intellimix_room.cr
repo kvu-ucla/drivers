8807b7cffee4bd1ef8ef829552f7c1411f3fcbea
Kenneth Vu <kvu@it.ucla.edu>
2026-02-06 09:19:48 -0800

    (TESTING) shards update imx room

--- drivers/shure/intellimix_room.cr
+++ drivers/shure/intellimix_room.cr
@@ -4,7 +4,6 @@ require "placeos-driver/interface/muteable"
 # Documentation: https://www.shure.com/en-US/docs/commandstrings/IntelliMixRoom
 # TCP Port: 2202
 
-
 class Shure::IntellimixRoom < PlaceOS::Driver
   include Interface::AudioMuteable
 
