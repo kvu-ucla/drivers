c6bb781ff48b6a689acbf6669010dd0da90104ab
Kenneth Vu <kvu@it.ucla.edu>
2026-05-06 18:43:36 -0700

    fix(zoom_zrc): check 301 redirect location

--- drivers/zoom/zoom_zrc.cr
+++ drivers/zoom/zoom_zrc.cr
@@ -244,6 +244,11 @@ class Zoom::ZRC::Controller < PlaceOS::Driver
 
   def get_health : JSON::Any
     response = get("/health", headers: JSON_HEADERS)
+    if response.status_code.in?(301, 302, 307, 308)
+      location = response.headers["Location"]?
+      raise "redirect with no location header" unless location
+      response = HTTP::Client.get(location)
+    end
     raise "request failed with #{response.status_code}" unless response.success?
     msg = JSON.parse(response.body)
     logger.debug { "#{msg}" }
